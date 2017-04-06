2de2afe0e95711c9a6d9d1b180ab441cfba64af4
Eric Dahlvang <v-erdahl@microsoft.com>
2017-04-05 17:19:35 -0700

    Update bottest.js

--- bottest.js
+++ bottest.js
@@ -1,3 +1,31 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+var port = 9042;
+
+var server = restify.createServer();
+server.use(restify.bodyParser());
+
+server.listen(port, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+var fs = require ("fs");
+var configFile = fs.readFileSync ("./testconfig.json");
+var config = JSON.parse (configFile);
+
+var connector = new builder.ChatConnector({
+    appId: config.appId,
+    appPassword: config.appPassword
+});
+
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', function (session) {    
+	session.send("yep1");
+});
+
+/*
 var express = require ("express");
 var expressServer = express ();
 var port = 9042;
@@ -37,5 +65,5 @@ bot.dialog ("/", function (session, args)
 		console.log ('yep1');
 	});
 
-
+*/
 
